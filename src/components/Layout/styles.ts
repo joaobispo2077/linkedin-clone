009ba97f565f72cd0a737aec8c26cb149fedfe80
João Bispo <vitor.vi10@hotmail.com>
2021-08-21 20:48:19 -0300

    fix: remove direct child selector

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -4,26 +4,26 @@ export const Container = styled.div`
   height: 100%;
   width: 100%;
 
-  & > .left-column,
-  & > .right-column {
+  .left-column,
+  .right-column {
     display: none;
   }
 
+  > main {
+    margin: 0 30px;
+
+    display: flex;
+    justify-content: center;
+  }
+
   @media (min-width: 1180px) {
-    & > .left-column,
-    & > .right-column {
+    .left-column,
+    .right-column {
       display: unset;
     }
 
-    & > .middle-column {
+    .middle-column {
       margin: 0 25px 16px;
-    }
-
-    > main {
-      margin: 0 30px;
-
-      display: flex;
-      justify-content: center;
     }
   }
 `;
