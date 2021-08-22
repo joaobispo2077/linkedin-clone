cb97148500822e8a9474c5abeee06ab71ce2b324
João Bispo <joaobispo2077@gmail.com>
2021-08-22 12:44:11 -0300

    feat: add  loading feed share structure

--- src/components/MiddleColumn/index.tsx
+++ src/components/MiddleColumn/index.tsx
@@ -5,13 +5,14 @@ import FeedPost from './FeedPost';
 
 import { Container } from './styles';
 import LoadingFeedPost from '../Shimmer/LoadingFeedPost';
+import LoadingFeedShare from '../Shimmer/LoadingFeedShare';
 
 const MiddleColumn: React.FC<LoadingProps> = ({ isLoading }) => {
   return (
     <Container className="middle-column">
       {isLoading ? (
         <>
-          {/* <LoadingFeedShare /> */}
+          <LoadingFeedShare />
           <LoadingFeedPost />
           <LoadingFeedPost />
           <LoadingFeedPost />
