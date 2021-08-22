07cf7ce44bc0dc13dfa417154536979169efa4c2
João Bispo <joaobispo2077@gmail.com>
2021-08-22 11:18:25 -0300

    feat: add sticky position into asides to stay fixed

--- src/components/RightColumn/styles.ts
+++ src/components/RightColumn/styles.ts
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 312px;
 
+  > div:nth-child(2) {
+    position: sticky;
+    top: 64px;
+  }
+
   > div + div {
     margin-top: 8px;
   }
