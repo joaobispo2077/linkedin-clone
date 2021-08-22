284f72b7023f1f0d1bb358c9dae223b940f3e2e1
João Bispo <joaobispo2077@gmail.com>
2021-08-22 13:06:26 -0300

    feat: show Adbanner after page loading

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -20,9 +20,7 @@ const Layout: React.FC = () => {
     <Container>
       <MobileHeader />
       <DesktopHeader />
-      <span>
-        <AdBanner />
-      </span>
+      <span>{!isLoading && <AdBanner />}</span>
       <main>
         <LeftColumn isLoading={isLoading} />
         <MiddleColumn isLoading={isLoading} />
